38b5153b17dbdadd5b4a3331d56e39456d5fb66f
David Portella <david@davidportella.cl>
2016-03-12 16:54:49 -0300

    feat(expense report) - Merge two tables in one

--- app/expense-report/controllers/ExpenseReportCtrl.js
+++ app/expense-report/controllers/ExpenseReportCtrl.js
@@ -10,80 +10,9 @@ angular
         // http://url_backend/v1/expense/report
         $scope.gridData = {
             caption: "EGRESOS",
-            hiddengrid: true,
-            data: [
-                {
-                    id: "1",
-                    tipoGasto: "ADMINISTRACION-REMUNERACIONES",
-                    detalle: "Sueldo Liquidos del Personal",
-                    documento: "Egreso #20",
-                    total: 1
-                },
-                {
-                    id: "2",
-                    tipoGasto: "ADMINISTRACION-REMUNERACIONES",
-                    detalle: "Previred",
-                    documento: "Egreso #22",
-                    total: 2
-                },
-                {
-                    id: "3",
-                    tipoGasto: "CONSUMO",
-                    detalle: "Articulos de aseo",
-                    documento: "Egreso #224",
-                    total: "123000"
-                },
-                {
-                    id: "4",
-                    tipoGasto: "CONSUMO",
-                    detalle: "Previred",
-                    documento: "Egreso #22",
-                    total: "733654"
-                },
-                {
-                    id: "5",
-                    tipoGasto: "MANTENCIONES",
-                    detalle: "Sueldo Liquidos del Personal",
-                    documento: "Egreso #20",
-                    total: "4790160"
-                },
-                {
-                    id: "6",
-                    tipoGasto: "MANTENCIONES",
-                    detalle: "Previred",
-                    documento: "Egreso #22",
-                    total: "733654"
-                },
-                {
-                    id: "7",
-                    tipoGasto: "REPARACIONES",
-                    detalle: "Sueldo Liquidos del Personal",
-                    documento: "Egreso #20",
-                    total: "4790160"
-                },
-                {
-                    id: "8",
-                    tipoGasto: "REPARACIONES",
-                    detalle: "Previred",
-                    documento: "Egreso #22",
-                    total: "733654"
-                },
-                {
-                    id: "9",
-                    tipoGasto: "VARIOS",
-                    detalle: "Sueldo Liquidos del Personal",
-                    documento: "Egreso #20",
-                    total: "4790160"
-                },
-                {
-                    id: "10",
-                    tipoGasto: "VARIOS",
-                    detalle: "Previred",
-                    documento: "Egreso #22",
-                    total: "733654"
-                }
-            ],
-            colNames: ['id', 'Tipo Gasto', 'Detalle', 'Documento', 'Total'],
+            hiddengrid: false,
+            hidegrid: false,
+            colNames: ['id', 'Tipo Gasto', 'Detalle', 'Documento', 'Ingreso', 'Egreso'],
             colModel: [
                 {
                     name: 'id',
@@ -94,7 +23,7 @@ angular
                     name: 'tipoGasto',
                     index: 'tipoGasto',
                     editable: true,
-                    edittype:"select",
+                    edittype: "select",
                     editoptions: {
                         value: 'ADMINISTRACION-REMUNERACIONES:ADMINISTRACION-REMUNERACIONES;CONSUMO:CONSUMO;MANTENCIONES:MANTENCIONES'
                     }
@@ -110,65 +39,106 @@ angular
                     editable: true
                 },
                 {
-                    name: 'total',
-                    index: 'total',
+                    name: 'ingreso',
+                    index: 'ingreso',
+                    align: "right",
+                    formatter: "integer",
+                    editable: true,
+                    summaryTpl: "Sum: {0}", // set the summary template to show the group summary
+                    summaryType: "sum" // set the formula to calculate the summary type
+                },
+                {
+                    name: 'egreso',
+                    index: 'egreso',
                     align: "right",
                     formatter: "integer",
                     editable: true,
                     summaryTpl: "Sum: {0}", // set the summary template to show the group summary
                     summaryType: "sum" // set the formula to calculate the summary type
                 }
-            ]
-        };
-
-        $scope.gridData6 = {
-            caption: "INGRESOS",
-            hiddengrid: true,
+            ],
             data: [
                 {
                     id: "1",
-                    tipoGasto: "GASTOS A COBRAR / DESCONTAR",
+                    tipoGasto: "ADMINISTRACION-REMUNERACIONES",
                     detalle: "Sueldo Liquidos del Personal",
                     documento: "Egreso #20",
-                    total: "4790160"
+                    ingreso: 1,
+                    egreso: 0
                 },
                 {
                     id: "2",
-                    tipoGasto: "GASTOS A COBRAR / DESCONTAR",
+                    tipoGasto: "ADMINISTRACION-REMUNERACIONES",
                     detalle: "Previred",
                     documento: "Egreso #22",
-                    total: "733654"
-                }
-            ],
-            colNames: ['id', 'Tipo Gasto', 'Detalle', 'Documento', 'Total'],
-            colModel: [
-                {
-                    name: 'id',
-                    index: 'id',
-                    hidden: true
-                },
-                { name: 'tipoGasto', index: 'tipoGasto'},
-                {
-                    name: 'detalle',
-                    index: 'detalle',
-                    editable: true
+                    ingreso: 2,
+                    egreso: 0
                 },
                 {
-                    name: 'documento',
-                    index: 'documento',
-                    editable: true
+                    id: "3",
+                    tipoGasto: "CONSUMO",
+                    detalle: "Articulos de aseo",
+                    documento: "Egreso #224",
+                    ingreso: "123000",
+                    egreso: 0
                 },
                 {
-                    name: 'total',
-                    index: 'total',
-                    align: "right",
-                    formatter: "integer",
-                    editable: true,
-                    summaryTpl: "Sum: {0}", // set the summary template to show the group summary
-                    summaryType: "sum" // set the formula to calculate the summary type
+                    id: "4",
+                    tipoGasto: "CONSUMO",
+                    detalle: "Previred",
+                    documento: "Egreso #22",
+                    ingreso: "733654",
+                    egreso: 0
+                },
+                {
+                    id: "5",
+                    tipoGasto: "MANTENCIONES",
+                    detalle: "Sueldo Liquidos del Personal",
+                    documento: "Egreso #20",
+                    ingreso: "4790160",
+                    egreso: 0
+                },
+                {
+                    id: "6",
+                    tipoGasto: "MANTENCIONES",
+                    detalle: "Previred",
+                    documento: "Egreso #22",
+                    ingreso: "733654",
+                    egreso: 0
+                },
+                {
+                    id: "7",
+                    tipoGasto: "REPARACIONES",
+                    detalle: "Sueldo Liquidos del Personal",
+                    documento: "Egreso #20",
+                    ingreso: "4790160",
+                    egreso: 0
+                },
+                {
+                    id: "8",
+                    tipoGasto: "REPARACIONES",
+                    detalle: "Previred",
+                    documento: "Egreso #22",
+                    ingreso: "733654",
+                    egreso: 0
+                },
+                {
+                    id: "9",
+                    tipoGasto: "VARIOS",
+                    detalle: "Sueldo Liquidos del Personal",
+                    documento: "Egreso #20",
+                    ingreso: "0",
+                    egreso: 5456
+                },
+                {
+                    id: "10",
+                    tipoGasto: "VARIOS",
+                    detalle: "Previred",
+                    documento: "Egreso #22",
+                    ingreso: 0,
+                    egreso: "733655"
                 }
             ]
         };
 
-        //$scope.breadOne = "Build ???";
     });
