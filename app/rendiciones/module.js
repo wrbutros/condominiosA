dd674292b329ef126c0e89e005c24429a7715455
Wilson Bustos <wrbutros@gmail.com>
2016-05-26 23:07:39 -0400

    fix(rendiciones) Se pone tabla de rendiciones en el module

--- app/rendiciones/module.js
+++ app/rendiciones/module.js
@@ -25,10 +25,86 @@ angular
                         var id_condominio = parseInt($stateParams.id_condominio);
                         var id_rendicion = parseInt($stateParams.id_rendicion);
 
-                        return Restangular
+                        // return Restangular
+                        //     .one("condominios", id_condominio)
+                        //     .one("rendiciones", id_rendicion)
+                        //     .get();
+
+                        /*TODO: Gets it from other site*/
+                        var table = [{
+                            "caption": "Gastos Comunes",
+                            "height": 400,
+                            "hiddengrid": false,
+                            "hidegrid": false,
+                            "grouping": true,
+                            "colNames": [
+                                "id",
+                                "Tipo Gasto",
+                                "Detalle",
+                                "Documento",
+                                "Ingreso",
+                                "Egreso"
+                            ],
+                            "colModel": [
+                                {
+                                    "name": "id",
+                                    "index": "id",
+                                    "hidden": true
+                                },
+                                {
+                                    "name": "tipoGasto",
+                                    "index": "tipoGasto",
+                                    "editable": true,
+                                    "edittype": "select",
+                                    "editoptions": {
+                                        "value": "ADMINISTRACION-REMUNERACIONES:ADMINISTRACION-REMUNERACIONES;CONSUMO:CONSUMO;MANTENCIONES:MANTENCIONES"
+                                    }
+                                },
+                                {
+                                    "name": "detalle",
+                                    "index": "detalle",
+                                    "editable": true
+                                },
+                                {
+                                    "name": "documento",
+                                    "index": "documento",
+                                    "editable": true
+                                },
+                                {
+                                    "name": "ingreso",
+                                    "index": "ingreso",
+                                    "align": "right",
+                                    "formatter": "integer",
+                                    "editable": true,
+                                    "fixed": true,
+                                    "width": 75,
+                                    "summaryTpl": "{0}",
+                                    "summaryType": "sum"
+                                },
+                                {
+                                    "name": "egreso",
+                                    "index": "egreso",
+                                    "align": "right",
+                                    "formatter": "integer",
+                                    "editable": true,
+                                    "fixed": true,
+                                    "width": 75,
+                                    "summaryTpl": "{0}",
+                                    "summaryType": "sum"
+                                }
+                            ],
+                            "data": [
+
+                            ]
+                        }];
+
+                        data =  Restangular
                             .one("condominios", id_condominio)
-                            .one("rendiciones", id_rendicion)
+                            .one("rendicionActual")
                             .get();
+
+                        table.data = data;
+                        return table;
 
                     },
                     scripts: function (lazyScript) {
